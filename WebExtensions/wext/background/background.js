b1af974021f798e1edfaceff92f8417162f793ca
PrsPrsBK <prsprsbk@gmail.com>
2019-04-09 01:48:25 +0900

    Maybe 4KiB + 1 'write' is necessary for stdout flush, on current ponyc

--- WebExtensions/wext/background/background.js
+++ WebExtensions/wext/background/background.js
@@ -1,15 +1,16 @@
 const port = browser.runtime.connectNative('pony_side');
 
 port.onMessage.addListener(response => {
-  console.log(`from pony: ${response}`);
+  console.log(`from pony: ${response.length} ${response}`);
 });
 
 browser.browserAction.onClicked.addListener(() => {
   browser.tabs.query({ currentWindow: true, active: true }).then(tabs => {
     for(const tab of tabs) {
-      let date = new Date();
-      console.log(`lets send ${date.toLocaleDateString()}-${date.toLocaleTimeString()} ${tab.url}`);
-      port.postMessage(`${date.toLocaleDateString()}-${date.toLocaleTimeString()} ${tab.url}`);
+      const date = new Date();
+      const message = `${date.toLocaleDateString()}-${date.toLocaleTimeString()} ${tab.url}`;
+      console.log(`lets send ${message}`);
+      port.postMessage(`${message}`);
     }
   });
 });
